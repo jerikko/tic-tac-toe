7c0d7cd3fe1125bac34f5ab53d6d492a863b2aea
Jerikko Agatep <jerikkoagatep@gmail.com>
2021-03-20 22:10:15 -0700

    Skeleton factory function & module

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,27 @@
+const Gameboard = () => {
+    let gameboard = ['X','O','X','O','X','O','X','O','X']
+}
 
+
+const Player = name => {                    // Factory Function
+    const getName = () => name
+
+}
+
+
+const displayController = (() => {
+    return {};
+})();
+
+// const calculator = (() => {              // Module
+//     const add = (a, b) => a + b;
+//     const sub = (a, b) => a - b;
+//     const mul = (a, b) => a * b;
+//     const div = (a, b) => a / b;
+//     return {
+//       add,
+//       sub,
+//       mul,
+//       div,
+//     };
+//   })();
